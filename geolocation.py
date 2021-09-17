99d4252c900bb41e64475748092fa5def81bcfef
Ruben Villalvazo <vim.ruben@hotmail.com>
2021-09-16 23:06:50 -0300

    OOP implementation

--- geolocation.py
+++ geolocation.py
@@ -1,17 +1,21 @@
 import requests
 
-datos  = {
-    "considerIp": "true"
-}
-
-api_key = "AIzaSyA-C2U9OWidR-wfrXZl5OEXbosiVVFOhC0"
-url = (f"https://www.googleapis.com/geolocation/v1/geolocate?key={api_key}")
-response = requests.post(url, json=datos)
-location = response.json()["location"]
-lat = location["lat"]
-lng = location["lng"]
-acuracy = response.json()["acuracy"]
-
-print(lat)
-print(lng)
-print(acuracy)
+class GEOLOCATION():
+    def __init__(self):
+        self.data  = {
+            "considerIp": "true"
+        }
+        self.api_key = "AIzaSyA-C2U9OWidR-wfrXZl5OEXbosiVVFOhC0"
+        self.url = (f"https://www.googleapis.com/geolocation/v1/geolocate?key={self.api_key}")
+        
+    def get_location(self):
+        response = requests.post(self.url, json=self.data)
+        location = response.json()["location"]
+        lat = location["lat"]
+        lng = location["lng"]
+        position = [lat, lng]
+        return position
+        
+if(__name__ == "__main__"):
+    geo = GEOLOCATION()
+    print(geo.get_location())
